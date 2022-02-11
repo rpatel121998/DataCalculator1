b129019caa5c245c8d21f66903eb006324c0dfc5
Sebastian <Sgonzalez@augusta.edu>
2022-02-11 17:08:09 -0500

    Working on Download

---
+++ DataCalculatorWebAppServer/Models/FileStorageData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataCalculatorWebAppServer.Models
+{
+    public class FileStorageData
+    {
+        [Key]
+        [ForeignKey("FileSendData")]
+        public int FileSendDataId { get; set; }
+        public FileSendData FileSendData { get; set; }
+
+        [DataType(DataType.Url)]
+        public string FileUri { get; set; }
+    }
+}
