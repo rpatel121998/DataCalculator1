d28c63f6c0b6a57c7764ca52b0c2b6d03074f0bf
AJ Chiong <anselmojosechiong2000@gmail.com>
2022-05-03 23:41:50 -0400

    preliminary push

---
+++ DataCalculatorWebAppServer/wwwroot/chart.js
@@ -0,0 +1,12 @@
+﻿var state = 0;
+
+var chart = null;
+
+window.setup = (id, config) => {
+
+    var ctx = document.getElementById(id).getContext('2d');
+
+    chart = new Chart(ctx, config);
+
+}
+
